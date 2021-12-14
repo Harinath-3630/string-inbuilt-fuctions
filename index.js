632a6d774fa9d2dbe4c1cf4f4affbeceb1d8ca4a
Harinath <kathiarun3630@gmail.com>
2021-12-14 19:59:46 +0530

    string inbuilt

---
+++ index.js
@@ -0,0 +1,37 @@
+var myStr="java script is a backend lang";
+console.log(myStr.length);
+
+var myStr="java script is a backend lang";
+console.log(myStr.indexOf('script'));
+
+var myStr="java script is a backend script lang";
+console.log(myStr.lastIndexOf('script'));
+
+
+var str="java script is a backend script lang";
+console.log(str.slice(0,6));
+console.log(str.slice(-9))
+console.log(str.slice(8))
+
+var myStr="java script is a backend script lang";
+console.log(str.substr(5,9));
+
+var myStr="java script is a backend script lang";
+console.log(str.toUpperCase(myStr));
+
+var myStr="JAVA SCRIPT IS A BACKEND SCRIPT LANG";
+console.log(str.toLowerCase(myStr));
+
+var firstStr="java";
+var secondStr="script";
+console.log(firstStr.concat(' ',secondStr));
+
+var myStr="      hai     ";
+console.log(myStr.trim());
+
+
+var str="JAVA SCRIPT IS A BACKEND SCRIPT LANG";
+console.log(str.charAt(5));
+
+var str="JAVA SCRIPT IS A BACKEND SCRIPT LANG";
+console.log(str.split('',));
